b235a7cbd52f3f527d8dfb3b25015035ffe5b61a
Edmond Meinfelder <edmond@stdarg.com>
2013-10-24 13:24:51 -0700

    Added argument checking.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict';
 var debug = require('debug')('obj-to-json');
 var is = require('is2');
+var inspect = require('util').inspect;
 
 /**
  * A convenience function to copy an object. Must be an object that can be
  * serialized into JSON.
  * @param {Object} obj An object to be copied.
- * @param {Array|Str} delProps A string or an array of properties to delete.
- * @return {Object|Boolean} returns the object copy or false, if there was an
- *   error.
- * @private
+ * @param {Array|Str} [delProps] A string or an array of properties to delete. Optional.
+ * @return {Object|Boolean} returns the object copy or false, if there was an error.
  */
 exports.copyObj = function copyObj(obj, delProps) {
+  if (!is.obj(obj)) {
+    debug('copyObj received bad obj param: '+inspect(obj));
+    return false;
+  }
+
   var newObj;
   try {
     newObj = JSON.parse(JSON.stringify(obj));
@@ -36,11 +40,14 @@ exports.copyObj = function copyObj(obj, delProps) {
 /**
  * A convenience function to convert a JSON string to an object.
  * @param {String} str A stringified JSON representation.
- * @return {Object|Boolean} returns the object representation of the JSON, if
- * the str representation is is legal and false otherwise.
- * @private
+ * @return {Object|Boolean} returns the object representation of the JSON, if the str representation is is legal and false otherwise.
  */
 exports.jsonStrToObj = function jsonStrToObj(str) {
+  if (!is.str(str)) {
+    debug('jsonStrToObj received bad str param: '+inspect(str));
+    return false;
+  }
+
   var obj;
   try {
     obj = JSON.parse(str);
@@ -55,9 +62,13 @@ exports.jsonStrToObj = function jsonStrToObj(str) {
  * A convenience function to convert an object to a JSON string.
  * @param {Object} obj A javascript object than can be converted to JSON
  * @return {String|Boolean} The JSON string if possible and false otherwise.
- * @private
  */
 exports.objToJsonStr = function objToJsonStr(obj) {
+  if (!is.obj(obj)) {
+    debug('objToJsonStr received bad param obj: '+inspect(obj));
+    return false;
+  }
+
   var str;
   try {
     str = JSON.stringify(obj);
